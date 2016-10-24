c0eee6a876e84a4f5d7ea9a1a7fe0a8e47838312
Alexey Zhuchkov <zer0divisi1@gmail.com>
2016-10-24 23:45:01 +0300

    Add termination sequence check to line split func

--- fix/code/src/cyberGo/main/handler.go
+++ fix/code/src/cyberGo/main/handler.go
@@ -435,6 +435,9 @@ func splitLines(data []byte, atEOF bool) (advance int, token []byte, err error) 
 		// We have a full newline-terminated line.
 		return i + 1, data[0:i], nil
 	}
+	if len(data) >= 3 && (string(data[len(data)-3:]) == "***") {
+		return len(data), data, bufio.ErrFinalToken
+	}
 	// If we're at EOF, we have a final, non-terminated line. Return it.
 	if atEOF {
 		return len(data), data, nil
